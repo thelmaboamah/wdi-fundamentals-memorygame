02aa22c054e737e939a1d9252d0600b89be4fa0a
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-01-18 23:32:11 -0800

    create divs with JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,4 +3,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
- 
+// if (cardTwo === cardFour || cardTwo === cardFour || cardOne === cardThree || cardOne === cardFour) {
+// 	alert("Not a match, try again!");
+// } else {
+// 	alert("You have a match");
+// } 
+
+var gameBoard = document.getElementById("game-board");
+
+function createCards() {
+	for(var i = 0; i < 4; i+=1) {
+		var div = document.createElement('div');
+		div.className = 'card';
+		gameBoard.appendChild(div);
+	}
+}
+
+createCards();
