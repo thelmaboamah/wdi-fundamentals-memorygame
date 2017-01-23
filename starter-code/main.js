9202c8df7ed4ba316e50b391c4308ea567ede1d9
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-01-23 02:08:19 -0800

    technically finished

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,43 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-// if (cardTwo === cardFour || cardTwo === cardFour || cardOne === cardThree || cardOne === cardFour) {
-// 	alert("Not a match, try again!");
-// } else {
-// 	alert("You have a match");
-// } 
+var cards = ['queen', 'queen', 'king', 'king']
+var cardsInPlay = []
 
 var gameBoard = document.getElementById("game-board");
 
-function createCards() {
-	for(var i = 0; i < 4; i+=1) {
+function createBoard() {
+	for(var i = 0; i < cards.length; i++) {
 		var div = document.createElement('div');
 		div.className = 'card';
+		div.setAttribute('data-card', cards[i]);
+		div.addEventListener('click', isTwoCards);
 		gameBoard.appendChild(div);
+	}
+
+}
+
+createBoard();
+
+function isTwoCards() {
+
+	var dataCard = this.getAttribute('data-card');
+	cardsInPlay.push(dataCard);
+	
+	if (dataCard == 'queen') {
+		this.innerHTML = '<img src="queen.png" alt = "queen of spades"/>'
+	} else {
+		this.innerHTML = '<img src="king.png" alt = "queen of spades"/>'
+	}
+
+
+	if (cardsInPlay.length == 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
 	}
 }
 
-createCards();
+function isMatch(arr) {
+	if (arr[0] == arr[1]) {
+		alert("It's a match!");
+	} else {
+		alert("Try again.");
+	}
+}
